adc927e85e361b1727b934369662b4316f201a6c
Alex Ginns <alexginns@gmail.com>
2019-07-23 21:20:30 +1000

    chore: initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,52 @@
+export default {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'Easy Read Time',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [
+
+    ]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+}
