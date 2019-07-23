9c0ab41be9873449abd9ca7d10bb251c35686573
Alex Ginns <alexginns@gmail.com>
2019-07-23 21:25:28 +1000

    feat: add global styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,37 +1,42 @@
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
    ** Headers of the page
    */
   head: {
-    title: 'Easy Read Time',
-    meta: [{
-        charset: 'utf-8'
+    title: "Easy Read Time",
+    meta: [
+      {
+        charset: "utf-8"
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
       },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
       }
     ],
     link: [
-
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Merriweather|Montserrat:900|Ubuntu+Mono&display=swap"
+      }
     ]
   },
   /*
    ** Customize the progress-bar color
    */
   loading: {
-    color: '#fff'
+    color: "#fff"
   },
   /*
    ** Global CSS
    */
-  css: [],
+  css: ["~/assets/styles/global.scss"],
   /*
    ** Plugins to load before mounting the App
    */
@@ -44,9 +49,10 @@ export default {
    ** Build configuration
    */
   build: {
+    transpile: ["easy-read-time"],
     /*
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
   }
-}
+};
